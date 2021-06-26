0cb30081dac100add67f09608542a75c77777ec6
kangdongil <67248006+kangdongil@users.noreply.github.com>
2021-06-26 10:01:33 +0900

    #23.8 Creating Room part One

--- rooms/models.py
+++ rooms/models.py
@@ -107,8 +107,11 @@ class Room(core_models.TimeStampedModel):
         super().save(*args, **kwargs)  # Call the real save() method
 
     def first_photo(self):
-        (photo,) = self.photos.all()[:1]
-        return photo.file.url
+        try:
+            (photo,) = self.photos.all()[:1]
+            return photo.file.url
+        except ValueError:
+            return None
 
     def get_preview_photos(self):
         photos = self.photos.all()[1:5]
