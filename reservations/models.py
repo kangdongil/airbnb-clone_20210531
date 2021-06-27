4f675aa197c87e64b58c27d583a4c2299447777b
kangdongil <67248006+kangdongil@users.noreply.github.com>
2021-06-27 19:43:26 +0900

    #24.16 Validate Everything

--- reservations/models.py
+++ reservations/models.py
@@ -2,7 +2,6 @@ import datetime
 from django.db import models
 from django.utils import timezone
 from core import models as core_models
-from . import managers
 
 
 class BookedDay(core_models.TimeStampedModel):
@@ -43,7 +42,6 @@ class Reservation(core_models.TimeStampedModel):
     room = models.ForeignKey(
         "rooms.Room", on_delete=models.CASCADE, related_name="reservations"
     )
-    objects = managers.CustomReservationManager()
 
     def in_progress(self):
         now = timezone.now().date()
