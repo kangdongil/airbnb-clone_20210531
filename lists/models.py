86167bb37edb26906e9325df5f2f48f5bbaa2b40
kangdongil <67248006+kangdongil@users.noreply.github.com>
2021-07-04 20:09:28 +0900

    #25.5 Fav Template Tag

--- lists/models.py
+++ lists/models.py
@@ -7,7 +7,7 @@ class List(core_models.TimeStampedModel):
     """List Model Definition"""
 
     name = models.CharField(max_length=80)
-    user = models.ForeignKey(
+    user = models.OneToOneField(
         "users.User", on_delete=models.CASCADE, related_name="lists"
     )
     rooms = models.ManyToManyField("rooms.Room", blank=True, related_name="lists")
