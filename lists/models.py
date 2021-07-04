f25aeaffbfecad1c11c65012e0fa5eed29a34a16
kangdongil <67248006+kangdongil@users.noreply.github.com>
2021-07-04 20:29:37 +0900

    #25.6 Fav Page

--- lists/models.py
+++ lists/models.py
@@ -8,7 +8,7 @@ class List(core_models.TimeStampedModel):
 
     name = models.CharField(max_length=80)
     user = models.OneToOneField(
-        "users.User", on_delete=models.CASCADE, related_name="lists"
+        "users.User", on_delete=models.CASCADE, related_name="list"
     )
     rooms = models.ManyToManyField("rooms.Room", blank=True, related_name="lists")
 
